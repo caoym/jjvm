0b25df385ed5e0c30a4d62139646b292401a2aa1
caoym <caoyangmin@gmail.com>
2017-09-30 11:20:18 +0800

    initialization of class&object

---
+++ src/main/java/org/caoym/jjvm/lang/JvmObject.java
@@ -0,0 +1,7 @@
+package org.caoym.jjvm.lang;
+
+/**
+ * Created by caoyangmin on 2017/9/29.
+ */
+public class JvmObject {
+}
---
+++ src/main/java/org/caoym/jjvm/natives/JvmNativeObject.java
@@ -0,0 +1,7 @@
+package org.caoym.jjvm.natives;
+
+/**
+ * Created by caoyangmin on 2017/9/29.
+ */
+public class JvmnativeObject {
+}
